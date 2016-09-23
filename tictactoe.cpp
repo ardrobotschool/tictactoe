8448ff31783a31e428b31ab63f90f9cba373513e
U-BSD\405502 <405502@SHS-10D8A6871.beaverton.k12.or.us>
2016-09-23 14:22:57 -0700

    change board to char array; finish printBoard function

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,17 +2,28 @@
 
 using namespace std;
 
-void printBoard(int board[3][3]);
+void printBoard(char (&board)[3][3]);
 
 int main(){
-  int board[3][3];
+  char board[3][3];
+  //Initialize empty board:
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+      board[i][j] = ' ';
+    }
+  }
+  board[2][2] = 'X';
   printBoard(board);
 }
 
 
-void printBoard(int board[3][3]){
+void printBoard(char (&board)[3][3]){
   cout << "  1 2 3" <<endl;
-  for(int i = 0; i < 3; i++){//Rows
-    
+  for(int i=0; i<3; i++){//rows
+    cout << (char)('A' + i) << " ";
+    for(int j=0; j<3; j++){//columns
+      cout << board[i][j] << " ";
+    }
+    cout << endl;
   }
 }
