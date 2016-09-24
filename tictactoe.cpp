fa77ded34cd7b130da9beefb5c12c09c73d6cd0a
Artur Drobot <405502@bsd48.org>
2016-09-23 17:22:26 -0700

    add moves and turns

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
+#include <ctype.h>
 
 using namespace std;
 
 void printBoard(char (&board)[3][3]);
+bool fullBoard(char (&board)[3][3]);
 
 int main(){
+  cout << "Enter moves in the format <row><column> (for example, \"b3\".)" << endl;
+  cout << "Enter \"q\" to quit at any time." << endl;
   char board[3][3];
+  char turn = 'X';
+  char input[3];
+  
   //Initialize empty board:
   for(int i=0; i<3; i++){
     for(int j=0; j<3; j++){
       board[i][j] = ' ';
     }
   }
-  board[2][2] = 'X';
+
+  while(!fullBoard(board)){//Single game here
+    printBoard(board);
+    cout << turn << "\'s turn. Enter move:";
+    cin >> input;
+    cout << endl;
+    if(input[0] == 'q'){
+      break;
+    }
+    board[tolower(input[0]) - 'a'][input[1]-'1'] = turn;
+    //Switch turns
+    if(turn == 'X'){
+      turn = 'O';
+    }
+    else{
+      turn = 'X';
+    }
+  }
   printBoard(board);
 }
 
@@ -26,4 +50,15 @@ void printBoard(char (&board)[3][3]){
     }
     cout << endl;
   }
+}
+
+bool fullBoard(char (&board)[3][3]){
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+      if(board[i][j] == ' '){
+	return false;
+      }
+    }
+  }
+  return true;
 }
