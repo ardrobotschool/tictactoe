7846f764b39dadf9cf21c1aea5c41b60530bd64a
Artur Drobot <405502@bsd48.org>
2016-09-23 17:49:37 -0700

    Add checkWin

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 void printBoard(char (&board)[3][3]);
 bool fullBoard(char (&board)[3][3]);
-
+bool checkWin(char(&board)[3][3], char player);
 int main(){
   cout << "Enter moves in the format <row><column> (for example, \"b3\".)" << endl;
   cout << "Enter \"q\" to quit at any time." << endl;
   char board[3][3];
   char turn = 'X';
   char input[3];
+  bool win = false;
   
   //Initialize empty board:
   for(int i=0; i<3; i++){
@@ -20,7 +21,7 @@ int main(){
     }
   }
 
-  while(!fullBoard(board)){//Single game here
+  while(!fullBoard(board) && !win){//Single game here
     printBoard(board);
     cout << turn << "\'s turn. Enter move:";
     cin >> input;
@@ -29,6 +30,7 @@ int main(){
       break;
     }
     board[tolower(input[0]) - 'a'][input[1]-'1'] = turn;
+    win = checkWin(board, turn);
     //Switch turns
     if(turn == 'X'){
       turn = 'O';
@@ -61,4 +63,26 @@ bool fullBoard(char (&board)[3][3]){
     }
   }
   return true;
+}
+
+bool checkWin(char (&board)[3][3], char player){
+  //Horizontals and verticals:
+  for(int i=0; i<3; i++){
+    if(board[i][0] == player && board[i][1] == player && board[i][2] == player){
+      return true;
+    }
+    if(board[0][i] == player && board[1][i] == player && board[2][i] == player){
+      return true;
+    }
+  }
+  //Diagonals:
+  //Seriously 3x3 is the threshold for deciding whether I should loop or manually check.
+  if(board[0][0] == player && board[1][1] == player && board[2][2] == player){
+    return true;
+  }
+  if(board[2][0] == player && board[1][1] == player && board[0][2] == player){
+    return true;
+  }
+  
+  return false;
 }
